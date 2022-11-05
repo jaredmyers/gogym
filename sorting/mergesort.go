dd89e46838a3d2b8565ae994ebdd6f3e4eed79fa
Jared Myers <jaredjmyers@gmail.com>
2022-11-05 11:51:23 -0400

    mergesort

---
+++ sorting/mergesort.go
@@ -0,0 +1,39 @@
+// golang
+// mergesort
+
+package sorting
+
+func mergeSort(items []int) []int {
+
+	if len(items) < 2 {
+		return items
+	} //base case
+
+	first := mergeSort(items[:len(items)/2])
+	second := mergeSort(items[len(items)/2:])
+
+	return merge(first, second)
+}
+
+func merge(a, b []int) []int {
+
+	final := []int{}
+	i := 0
+	j := 0
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			final = append(final, a[i])
+			i++
+		} else {
+			final = append(final, b[j])
+			j++
+		}
+	}
+	for ; i < len(a); i++ {
+		final = append(final, a[i])
+	}
+	for ; j < len(b); j++ {
+		final = append(final, b[j])
+	}
+	return final
+}
